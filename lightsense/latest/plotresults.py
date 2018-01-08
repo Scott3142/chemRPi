4779242cb0745dac1985afd3cf0dce433686a5d6
Scott Morgan <MorganSN@cardiff.ac.uk>
2018-01-08 17:20:05 +0000

    added scipy to plot function

--- lightsense/latest/plotresults.py
+++ lightsense/latest/plotresults.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
+from scipy import stats
 
-xarray = [2,4]
-yarray = [0.3922838,0.368606]
+xarray = [2,3,4]
+yarray = [0.3922838,0.379635,0.368606]
 
 plt.plot(xarray,yarray,marker='o',color='k',markerfacecolor='g')
 plt.plot(xarray,yarray,color='r',linewidth=2)
